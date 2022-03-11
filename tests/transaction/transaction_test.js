c33b9c71c2c786302d96f20774fb2cffae6bd860
jimboj <jamesdjohnson218@gmail.com>
2022-03-11 12:23:47 -0700

    connect to alice

--- tests/transaction/transaction_test.js
+++ tests/transaction/transaction_test.js
@@ -1,15 +1,26 @@
 // Import
-const { ApiPromise, WsProvider } = require('@polkadot/api');
+const { ApiPromise, WsProvider, HttpProvider} = require('@polkadot/api');
 //const {Keyring } = require('@polkadot/keyring');
 
-
+// 7001/tcp 8545/tcp 8546/tcp 8540/tcp 9876/tcp 6060/tcp
+//172.19.0.2
 async function main () {
     // Construct
-    const wsProvider = new WsProvider('wss://rpc.polkadot.io');
-    const api = await ApiPromise.create({ provider: wsProvider });
+    //const wsProvider = new WsProvider('wss://rpc.polkadot.io');
+    const wsProvider = new WsProvider('ws://127.0.0.1:8546');
+
+    // Create the instance
+    const api = new ApiPromise({ provider: wsProvider });
+
+// Wait until we are ready and connected
+    await api.isReady;
 
 // Do something
     console.log(api.genesisHash.toHex());
+
+
+    const runtimeVersion = await api.runtimeVersion;
+    console.log(`runtime version: ${runtimeVersion}`);
 }
 
 main().then(() => console.log('completed'))
