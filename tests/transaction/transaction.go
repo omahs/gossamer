a793694d8cc6a3e888bfa6c71d80c2157811ea2e
jimboj <jamesdjohnson218@gmail.com>
2022-03-18 16:20:11 -0600

    use different branch for go script

---
+++ tests/transaction/transaction.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	gsrpc "github.com/centrifuge/go-substrate-rpc-client/v3"
+)
+
+func main() {
+	api, err := gsrpc.NewSubstrateAPI("ws://127.0.0.1:8546")
+	if err != nil {
+		fmt.Println(err)
+	}
+	hash, err := api.RPC.Chain.GetBlockHashLatest()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Hash: ", hash)
+}
